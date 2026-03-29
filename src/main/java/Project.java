73536d8c19bb99d430d0e7646d8df5285dd0f96c
yongyong <iopuhjrybe57@gmail.com>
2026-03-29 22:49:30 +0900

    Initial commit

---
+++ src/main/java/Project.java
@@ -0,0 +1,20 @@
+package com.weaver.backend;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String category;
+
+    @Column(length = 1000)
+    private String status;
+    private String link;
+}
